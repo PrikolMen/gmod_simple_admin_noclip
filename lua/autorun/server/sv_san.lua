f8dfe3846c98cbf5ffc380c0a42e844db4d03502
PrikolMen <olegis019@gmail.com>
2022-09-18 03:13:50 +0400

    Update sv_san.lua

--- lua/autorun/server/sv_san.lua
+++ lua/autorun/server/sv_san.lua
@@ -1,8 +1,13 @@
 -- Simple Admin Noclip (SAN)
-local dedicated = game.IsDedicated()
+local notDedicated = not game.IsDedicated()
+local singleplayer = game.SinglePlayer()
 hook.Add("PlayerNoClip", "Simple Admin Noclip", function( ply, enable )
+    if (singleplayer) then
+        return true
+    end
+
     if (enable) then
-        if ply:IsAdmin() and ply:IsFullyAuthenticated() or (dedicated) and ply:IsListenServerHost() then
+        if ply:IsAdmin() and ply:IsFullyAuthenticated() or (notDedicated) and ply:IsListenServerHost() then
             return true
         end
 
